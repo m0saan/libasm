211478c6738c6f052d4b33b8fb6d5825c3c9f0fb
MoBoustta <moboustta6@gmail.com>
2020-11-25 17:52:58 +0100

    Create a main.c to test the functions

--- main.c
+++ main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <memory.h>
 
 extern size_t ft_strlen(const char *s);
+extern  char *ft_strcpy(char * dst, const char * src);
 
 int main() {
-    //ft_strlen("Hello, World\n");
-    printf("%lu\n", strlen("fdjvgdagj;adgja;egj  fgja ejgojag;   ofjoewjO JEFW"));
-    printf("%lu\n", ft_strlen("fdjvgdagj;adgja;egj  fgja ejgojag;   ofjoewjO JEFW"));
+/*
+    printf("----------------------- ft_strlen ----------------------- \n");
+
+    printf("%lu\n", strlen("Hello, assembly JEFW\n"));
+    printf("%lu\n", ft_strlen("Hello, assembly JEFW\n"));
+*/
+    printf("----------------------- ft_strcpy ----------------------- \n");
+
+    char *src = "|Hello, assembly JEFW|\n";
+    char *dst = malloc((strlen(src) + 1) * sizeof(char));
+    char *dst1 = malloc((strlen(src) + 1) * sizeof(char));
+
+    printf("dst1: %s\n", ft_strcpy(dst1, src));
+    printf("dst: %s\n", strcpy(dst, src));
     return 0;
 }
