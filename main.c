9b24cfa96f10b02276a6d60340a95f53e9727698
MoBoustta <moboustta6@gmail.com>
2020-11-25 18:54:14 +0100

    testing strcm

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 
 extern size_t ft_strlen(const char *s);
 extern  char *ft_strcpy(char * dst, const char * src);
+extern  int ft_strcmp(const char *s1, const char *s2);
+
 
 int main() {
 /*
@@ -11,7 +13,7 @@ int main() {
 
     printf("%lu\n", strlen("Hello, assembly JEFW\n"));
     printf("%lu\n", ft_strlen("Hello, assembly JEFW\n"));
-*/
+
     printf("----------------------- ft_strcpy ----------------------- \n");
 
     char *src = "|Hello, assembly JEFW|\n";
@@ -20,5 +22,14 @@ int main() {
 
     printf("dst1: %s\n", ft_strcpy(dst1, src));
     printf("dst: %s\n", strcpy(dst, src));
+
+*/
+    printf("----------------------- ft_strcmp ----------------------- \n");
+
+    const char *str1 = "H";
+    const char *str2 = "H";
+
+    printf("%d\n", strcmp(str1, str2));
+    printf("%d\n", ft_strcmp(str1, str2));
     return 0;
 }
