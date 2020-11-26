741504041dc663d52f3f66139cde4aed12c302d6
MoBoustta <moboustta6@gmail.com>
2020-11-26 18:31:49 +0100

    write been implemented

--- ft_write.s
+++ ft_write.s
@@ -3,20 +3,20 @@ global      _ft_write
 section     .text
 
 _ft_write:
-    mov rbx, rdi
-    mov rcx, rsi
-    mov eax, rdx
+    mov rbx, rdi; put fd to rbx
+    mov rcx, rsi; put msg to rcx
+    mov r8, rdx; put msg len to
 
 
 write:
     mov rax, 0x2000004
-    mov rsi, rbx
-    mov rdi, rcx
-    mov rdx, r9b
+    mov rdi, rbx
+    mov rsi, rcx
+    mov rdx, r8
     syscall
     jmp exit
 
 exit:
-    mov rax, 0x200000
+    mov rax, 0x2000001
     mov rdi, 0
     syscall
