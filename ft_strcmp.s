e20ca6e1e96a021db3ee4146c5ca253c2619e4c4
MoBoustta <moboustta6@gmail.com>
2020-11-26 10:39:42 +0100

    fix strcmp

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -9,33 +9,27 @@ section .text
 
 _ft_strcmp:
     mov r8, -1
-    mov rax, 0
 
 compare:
     inc r8
     mov cl, byte [rdi + r8]
-    mov al, byte [rsi + r8]
+    cmp cl, byte [rsi + r8]
+
+    jg  IS_GREAT
+    jl  IS_LESS
 
     cmp cl, 0
-    je end
-
-    cmp al, 0
-    je end
-
-    cmp cl, al
-    jg  add_to_register
-    jl  sub_from_register
+    je IS_EQUAL
     jmp compare
 
+IS_EQUAL:
+    mov rax, 0
+    ret
 
-add_to_register:
-    movsx rdx, cl
-    add rax, rdx
+IS_LESS:
+    mov rax, -1
+    ret
 
-sub_from_register:
-    movsx rdx, cl
-    sub rax, rdx
-
-
-end:
+IS_GREAT:
+    mov rax, 1
     ret
