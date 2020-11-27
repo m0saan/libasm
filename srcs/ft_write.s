f895c9cfe528365b7eb0b031d9dafb2c5e5d8c04
MoBoustta <moboustta6@gmail.com>
2020-11-27 09:15:48 +0100

    implement write

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -3,20 +3,8 @@ global      _ft_write
 section     .text
 
 _ft_write:
-    mov rbx, rdi; put fd to rbx
-    mov rcx, rsi; put msg to rcx
-    movsx r8, rdx; put msg len to
-    jmp write
-
-write:
+    mov rbx, rdx
     mov rax, 0x2000004
-    mov rsi, rcx
-    mov rdi, 1
-    mov rdx, rdx
     syscall
-    jmp exit
-
-exit:
-    mov rax, 0x2000001
-    mov rdi, 0
-    syscall
+    mov rax, rbx
+    ret
