edb34e3de2330cdd8ab0ca2fb3c8fae8919b756f
MoBoustta <moboustta6@gmail.com>
2020-11-27 10:57:21 +0100

    handle errors in write

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -1,10 +1,29 @@
+SYS_WRITE_MAC equ 0x2000004
+
 global      _ft_write
+extern      ___error
 
 section     .text
 
-_ft_write:
+_ft_write:          ; rdi = file descriptor, rsi = string, rdx = string length
+    cmp rdx, 0      ; check if the str lenght is < 0
+    jl len_error    ; if len < 0 ==> call len_error
+
     mov rbx, rdx
-    mov rax, 0x2000004
+    mov rax, SYS_WRITE_MAC
     syscall
-    mov rax, rbx
+
+    jc error        ; error sets carry flag, rax = errno
+    mov rax, rbx    ; put the printed chars length in rax
+    ret             ; return rax
+
+len_error:
+    mov rax, -1
+    ret
+
+error:
+    mov rbx, rax     ; save errno
+    call ___error    ; rax is now points to external variable errno.
+    mov [rax], rbx
+    mov rax, 0
     ret
