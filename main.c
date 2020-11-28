05b1af7427f7d365db887052d380cd3a839098ed
MoBoustta <moboustta6@gmail.com>
2020-11-28 10:00:22 +0100

    add some test

--- main.c
+++ main.c
@@ -8,7 +8,8 @@ extern size_t ft_strlen(const char *s);
 extern char *ft_strcpy(char * dst, const char * src);
 extern int ft_strcmp(const char *s1, const char *s2);
 extern size_t ft_write(int fil_des, const void *buf, size_t n_byte);
-
+extern size_t ft_read(int fil_des, void *buf, size_t n_byte);
+extern char *ft_strdup(const char *s1);
 
 int main() {
 /*
@@ -25,24 +26,40 @@ int main() {
 
     printf("dst1: %s\n", ft_strcpy(dst1, src));
     printf("dst: %s\n", strcpy(dst, src));
-
+*/
     printf("----------------------- ft_strcmp ----------------------- \n");
 
-    const char *str1 = "H";
-    const char *str2 = "Hss";
+    //const char *str1 = "H";
+    //const char *str2 = "Hss";
 
-    printf("%d\n", strcmp(str1, str2));
-    printf("%d\n", ft_strcmp(str1, str2));
-*/
+    const char *s1 = "bon";
+    const char *s2 =  "";
+
+    printf("%d\n", strcmp(s1, s2));
+    printf("%d\n", ft_strcmp(s1, s2));
     printf("----------------------- ft_write ----------------------- \n");
-
+/*
     const char *s1 = "";
     const char *s2 = "Print this string to the standard output\n";
 
-    //printf("%lu",write(1, s1, strlen(s1)));
-    //printf("%lu",write(1, s2, strlen(s2)));
+    //printf("%zu\n", write(1, s2, -1));
+    //printf("Errno: %d\n", errno);
+    //printf("Error: %s\n", strerror(errno));
 
-    printf("%zu: \n", ft_write(1, s1, strlen(s1)));
-    printf("%zu: \n", ft_write(1, s2, strlen(s2)));
+    */
+
+/*----------------------------------------------------------------------------------*/
+
+    //printf("%zu\n", ft_write(1, s2, -1));
+    //printf("Errno: %d\n", errno);
+    //printf("Error: %s\n", strerror(errno));
+
+    //const char *s2 = "";
+    //char *rslt1 = (char*) malloc(sizeof(char) * strlen(s2) + 1);
+    //char *rslt2 = (char*) malloc(sizeof(char) * strlen(s2) + 1);
+    //rslt1 = strdup(s2);
+    //rslt2 = ft_strdup(s2);
+    //printf("|%s|\n", rslt1);
+    //printf("|%s|\n", rslt2);
     return 0;
 }
