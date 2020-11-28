d797c0190085fce9a2fba580fe24e1afd254d453
MoBoustta <moboustta6@gmail.com>
2020-11-28 09:58:31 +0100

    implement write

--- srcs/ft_write.s
+++ srcs/ft_write.s
@@ -1,10 +1,22 @@
+SYS_WRITE_MAC equ 0x2000004
+
 global      _ft_write
+extern      ___error
 
 section     .text
 
-_ft_write:
+_ft_write:          ; rdi = file descriptor, rsi = string, rdx = string length
     mov rbx, rdx
-    mov rax, 0x2000004
+    mov rax, SYS_WRITE_MAC
     syscall
-    mov rax, rbx
+
+    jc error        ; error sets carry flag, rax = errno
+    mov rax, rbx    ; put the printed chars length in rax
+    ret             ; return rax
+
+error:
+    push rax        ; save errno
+    call ___error   ; rax is now points to external variable errno.
+    pop qword[rax]  ; The QWORD PTR is just a size specifier (It means that a 64 bit value is read from the address)
+    mov rax, -1
     ret
