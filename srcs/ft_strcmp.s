3eac7491ea76748f6f3a1f655d8dfc4903085e30
MoBoustta <moboustta6@gmail.com>
2020-11-28 14:05:36 +0100

    fix the bug -> not comparing words

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -2,13 +2,22 @@
 ; s1 > s2 ==> int bigger than zero
 ; s1 < s2 ==> int less than zero
 ; s1 == s2 ==> int equal to zero
-
 global _ft_strcmp
 
 section .text
 
 _ft_strcmp:
+    xor r14b, r14b
+    xor r15b, r15b
     mov r11, 0
+    call compare
+
+check_byte:
+    mov r14b, byte[rdi + r11]
+    mov r15b, byte[rsi + r11]
+    cmp r14b, r15b
+    jg IS_GREAT
+    jl IS_LESS
 
 compare:
     mov r15, [rdi + r11]
