91790731e9fefb9ce7a58904abda625db7cd942b
MoBoustta <moboustta6@gmail.com>
2021-01-07 16:57:33 +0100

    add comments to code

--- srcs/ft_strlen.s
+++ srcs/ft_strlen.s
@@ -4,21 +4,16 @@ global _ft_strlen
 section .text
 
 _ft_strlen:
-    mov rax, rdi
+    mov rax, rdi ;move the content of rdi(which is the 1st param) to rax.
 
 iterate:
-    cmp byte[rax] , 0
-    je end
+    cmp byte[rax] , 0 ;check the current character is it equal NULL
+    je end ;end if it equal NULL. 
 
-    inc rax
+    inc rax ; else increment rax.
     jmp iterate
 
 end:
     sub rax, rdi
     ret
     call exit
-
-exit:
-    mov     rax, 0x2000001 ; exit
-    mov     rdi, 0
-    syscall
